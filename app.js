79872de2ca0d701e74f2a1601750ebe0a965f5dc
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-08-14 20:03:09 -0300

    Comentarios nos codigos

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+// importa os frameworks que serão utilizados
 var express = require('express');
 var routes = require('./routes');
 var load = require("express-load");
 var app = express();
 
+// seta configurações
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+// seta configurações de rotas
 app.get('/', routes.index);
 app.get('/usuarios', routes.user.index);
 
+// carrega os modulos que serão usados
+load.('models')
+    .then('controllers')
+    .then('routes')
+    .into(app);
+    
+// Coloca aplicação no ar
 app.listen(3000, function() {
     console.log("Aplicação no ar!");
 });
