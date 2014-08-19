21a9ca1f58b6af19313e474f8ed8fc537a3adb31
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-08-19 20:39:01 -0300

    Controle de session

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+app.use(express.cookieParser('app_poc_real_time'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 load('models')
--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,28 @@
-module.exports = function(app) {
+module.exports = function (app) {
     var HomeController = {
-        index: function(req, res) {
+        index: function (req, res) {
             res.render('home/index');
+        },
+
+        login: function (req, res) {
+            var email = req.body.usuario.email,
+                nome = req.body.usuario.nome;
+
+            if (email && nome) {
+                var usuario = req.body.usuario;
+                usuario['contato'] = [];
+                req.session.usuario = usuario;
+                res.redirect('/contatos');
+            } else {
+                res.redirect('/');
+            }
+        },
+        
+        logout: function(req, res) {
+            req.session.destroy();
+            res.redirect('/');
         }
     };
+
     return HomeController;
 };
