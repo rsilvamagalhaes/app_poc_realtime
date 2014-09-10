e94ccca5def6a56608a4e7fc9a11bcaa68d195ae
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-09-10 19:03:56 -0300

    Socket io

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express'),
+    app = express(),
     load = require('express-load'),
-    error = require('./middleware/error');
-var app = express();
+    error = require('./middleware/error'),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
+var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.cookieParser('app_poc_real_time'));
@@ -13,12 +16,12 @@ app.use(express.methodOverride());
 app.use(app.router)
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.status(404);
     res.render('not-found');
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.status(500);
     res.render('server-error', error);
 });
@@ -27,6 +30,14 @@ load('models')
     .then('controllers')
     .then('routes')
     .into(app);
+
+io.sockets.on('connection', function (client){
+    client.on('send-server', function (data) {
+        var msg = "<b>" + data.nome + ":</b> " + data.msg + "<br>";
+        client.emit('send-client', msg);
+        client.broadcast.emit('send-client', msg);
+    });
+});
 
 app.listen(3000, function () {
     console.log("Projeto no ar!");
